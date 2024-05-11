b9db83d9bb6c4e941eb4bd3ff135ef7d898f867f
Tsingzao <tsingzao@hotmail.com>
2024-05-11 10:53:08 +0800

    Add files via upload

---
+++ models/segformer/heads/__init__.py
@@ -0,0 +1,7 @@
+from .segformer import SegFormerHead
+from .fpn import FPNHead
+from .fapn import FaPNHead
+from .fcn import FCNHead
+from .condnet import CondHead
+
+__all__ = ['SegFormerHead', 'FPNHead', 'FaPNHead', 'FCNHead', 'CondHead']
